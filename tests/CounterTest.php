a963ef05f25a8b0ad8b71de911dc38992b215b4d
andrarstn <andrarstn17@gmail.com>
2023-11-12 07:57:23 +0700

    depends

--- tests/CounterTest.php
+++ tests/CounterTest.php
@@ -27,4 +27,24 @@ class CounterTest extends TestCase
         
         $this->assertEquals(3, $counter->getCounter());
     }
+
+    public function testFirst() : Counter 
+    {
+        $counter = new Counter();
+        $counter->increment();
+
+        $this->assertEquals(1, $counter->getCounter());
+
+        return $counter;
+    }
+
+    /**
+     * @depends testFirst
+     */
+    public function testSecond(Counter $counter) : void
+    {
+        $counter->increment();
+
+        $this->assertEquals(2, $counter->getCounter());
+    }
 }
